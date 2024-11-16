bd72ddb70cd4c2da8aa7b92ed73ddc4e3fda80e9
true1ann <true1ann@tutamail.com>
2024-11-16 18:15:12 +0300

    Update files

---
+++ src/core/ui/settings/pages/nc_commands/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function CommandBrowser() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Unimplemented</Text>
+        </View>
+    );
+}
