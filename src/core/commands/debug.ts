85970832886a2fcecf1dbc58b434e3e5e6c613af
true1ann <true1ann@tutamail.com>
2024-11-16 12:23:20 +0300

    Update files

--- src/core/commands/debug.ts
+++ src/core/commands/debug.ts
@@ -16,14 +16,13 @@ export default () => <ApplicationCommand>{
     execute([ephemeral], ctx) {
         const info = getDebugInfo();
         const content = [
-            "**Revenge Debug Info**",
-            `> NC: ${info.bunny.version} (${info.bunny.loader.name} ${info.bunny.loader.version})`,
+            "**NeoCord Debug Info**",
+            `> NeoCord: ${info.bunny.version} (${info.bunny.loader.name} ${info.bunny.loader.version})`,
             `> Discord: ${info.discord.version} (${info.discord.build})`,
             `> React: ${info.react.version} (RN ${info.react.nativeVersion})`,
             `> Hermes: ${info.hermes.version} (bcv${info.hermes.bytecodeVersion})`,
             `> System: ${info.os.name} ${info.os.version} ${info.os.sdk ? `(SDK ${info.os.sdk})` : ""}`.trimEnd(),
             `> Device: ${info.device.model} (${info.device.codename})`,
-            '**NC Made by `@true1ann`, Thank You for using it.**'
         ].join("\n");
 
         if (ephemeral?.value) {
