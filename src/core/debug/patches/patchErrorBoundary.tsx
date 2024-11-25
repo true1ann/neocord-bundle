c9f2f2322b4af9f773498bdbf2c342b24553516a
Annie <true1ann@tutamail.com>
2024-11-25 21:54:44 +0300

    Update patchErrorBoundary.tsx

--- src/core/debug/patches/patchErrorBoundary.tsx
+++ src/core/debug/patches/patchErrorBoundary.tsx
@@ -4,6 +4,7 @@ import { _lazyContextSymbol } from "@metro/lazy";
 import { LazyModuleContext } from "@metro/types";
 import { findByNameLazy } from "@metro/wrappers";
 import { settings } from "@lib/api/settings";
+import { logger } from "@lib/utils/logger";
 
 function getErrorBoundaryContext() {
     const ctxt: LazyModuleContext = findByNameLazy("ErrorBoundary")[_lazyContextSymbol];
@@ -11,6 +12,8 @@ function getErrorBoundaryContext() {
 }
 
 export default function patchErrorBoundary() {
+    logger.log('Attempting to get Patching status..')
+    logger.log(settings.doPatchErrorBoundary)
     if (settings.doPatchErrorBoundary) {
         return after.await("render", getErrorBoundaryContext(), function (this: any) {
             if (!this.state.error) return;
