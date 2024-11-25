2b24cad79089c07fb1efda8d71c5bdef7618a796
Annie <true1ann@tutamail.com>
2024-11-25 12:40:04 +0300

    Update patchErrorBoundary.tsx

--- src/core/debug/patches/patchErrorBoundary.tsx
+++ src/core/debug/patches/patchErrorBoundary.tsx
@@ -14,7 +14,7 @@ export default function patchErrorBoundary() {
     const settings = useProxy();
     
     if (!settings.doPatchErrorBoundary) {
-        return;
+        return <ErrorBoundaryScreen />;
     }
 
     return after.await("render", getErrorBoundaryContext(), function (this: any) {
