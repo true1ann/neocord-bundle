af0ce3c435350086bf330152c1792552acca8c7e
Annie <true1ann@tutamail.com>
2024-11-25 13:39:42 +0300

    Update patchErrorBoundary.tsx

--- src/core/debug/patches/patchErrorBoundary.tsx
+++ src/core/debug/patches/patchErrorBoundary.tsx
@@ -3,6 +3,7 @@ import { after } from "@lib/api/patcher";
 import { _lazyContextSymbol } from "@metro/lazy";
 import { LazyModuleContext } from "@metro/types";
 import { findByNameLazy } from "@metro/wrappers";
+import { settings } from "@lib/api/settings";
 
 function getErrorBoundaryContext() {
     const ctxt: LazyModuleContext = findByNameLazy("ErrorBoundary")[_lazyContextSymbol];
@@ -10,12 +11,16 @@ function getErrorBoundaryContext() {
 }
 
 export default function patchErrorBoundary() {
-   return after.await("render", getErrorBoundaryContext(), function (this: any) {
-        if (!this.state.error) return;
+    if (settings.doPatchErrorBoundary) {
+        return after.await("render", getErrorBoundaryContext(), function (this: any) {
+            if (!this.state.error) return;
 
-        return <ErrorBoundaryScreen
-            error={this.state.error}
-            rerender={() => this.setState({ info: null, error: null })}
-        />;
-    });
+            return <ErrorBoundaryScreen
+                error={this.state.error}
+                rerender={() => this.setState({ info: null, error: null })}
+            />;
+        });
+    } else {
+        return;
+    }
 }
