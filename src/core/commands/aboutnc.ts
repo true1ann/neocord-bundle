72a99e62bf9b008e41ef7b2b20b4507e3b7bdf8a
Annie <true1ann@tutamail.com>
2024-11-27 02:24:29 +0300

    Create aboutnc.ts

---
+++ src/core/commands/aboutnc.ts
@@ -0,0 +1,21 @@
+import { ApplicationCommand, ApplicationCommandOptionType } from "@lib/api/commands/types";
+import { messageUtil } from "@metro/common";
+
+export default () => <ApplicationCommand>{
+    name: "aboutnc",
+    description: "Tell people NCs About and send a download link",
+    options: [],
+    execute(ctx) {
+        const content = [
+            "**What is NeoCord?**",
+            "NeoCord is a fork of Revenge, with additional and experimental features.",
+            "Some of 'Experimental features':",
+            "- Permissions",
+            "- Loaders",
+            "More will come over time.",
+            "You can read more and download it [here](https://github.com/true1ann/neocord-bundle) (GitHub)."
+        ].join("\n");
+
+        messageUtil.sendMessage(ctx.channel.id, { content });
+    }
+};
